87363313d4f6bcdcd3b0900bbedd17bae65688fb
Mateus Otavio Maia <57602069+momaia@users.noreply.github.com>
2024-10-01 16:45:44 -0300

    [ADD] Intalando react router dom

---
+++ src/pages/SingIn/index.js
@@ -0,0 +1,8 @@
+export default function SingIn() {
+    return (
+      <div>
+        <h1>Pagina Login</h1>
+      </div>
+    );
+  }
+  
---
+++ src/pages/SingUp/index.js
@@ -0,0 +1,8 @@
+export default function SingUp() {
+    return (
+      <div>
+        <h1>Pagina Cadastro</h1>
+      </div>
+    );
+  }
+  
